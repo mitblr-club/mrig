a67e40b7ca9a316e87784248aa61ec34402e8a06
Abhigyan Trips <abhigyantrips@gmail.com>
2024-09-19 15:18:31 +0530

    chore(project): init site config

---
+++ src/config/site.ts
@@ -0,0 +1,22 @@
+import { NavItem } from "@/types";
+
+export const siteConfig = {
+	name: 'MRIG',
+	institution: 'Manipal Institute of Technology, Bengaluru',
+	description: '',
+	projectsTableId: '',
+	navLinks: [
+		{
+			title: 'Projects',
+			href: '/projects',
+		}
+	] satisfies NavItem[],
+	socials: {
+		instagram: '',
+		linkedin: '',
+		github: '',
+	},
+	contact: {
+		email: '',
+	}
+};
